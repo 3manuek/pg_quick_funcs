6b4f3aaac816629d3469cb82f950aed100e08eae
Emanuel <3manuek@esdebian.org>
2014-07-18 18:13:35 -0300

    Update INSTALL_pg_quick_funcs.sql

--- INSTALL_pg_quick_funcs.sql
+++ INSTALL_pg_quick_funcs.sql
@@ -70,12 +70,16 @@ BEGIN
        FOR r IN select psd.*, 
                   pg_size_pretty(pg_database_size(datname)) as size 
                 from pg_database pd join pg_stat_database psd using (datname)
+                where psd.datname NOT IN ('template0','template1')
                 order by pg_database_size(datname) desc
        LOOP
               output_text = output_text || 
                  $$========================================== 
-                 $$ || 'Database: ' || (r.*).datname || ' (id):' || (r.*).datid || $$
-                 $$ || 'Current Backends ' || (r.*).numbackends || $$ 
+                 $$ || 'Database: ' || (r.datname) || ' (id):' || (r.datid)::text || $$
+                 $$ || 'Current Backends ' || (r.numbackends)::text || $$ 
+                 $$ || 'Size :           ' || (r.size)::text || $$ 
+                 $$ || 'Comit/Rollback : ' || (r.xact_commit)::text || '/' || (r.xact_rollback)::text || $$
+                 $$ || 'Blks Read/hit  : ' || (r.blks_read)::text || '/' || (r.blks_hit)::text || $$
                  $$ || (r.*)::text || $$
                  $$;
        END LOOP;
